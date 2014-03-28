be89cbfb54a8de2dea488bc82b0df437d9da8329
Benjamin Doherty <bjd@bangpound.org>
2014-03-28 16:57:52 -0500

    add cmf routing bundle to kernel.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
             new Bangpound\Bundle\DrupalBundle\BangpoundDrupalBundle(),
         );
 
